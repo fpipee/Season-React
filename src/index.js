895176fdc53042652fb91780942db8c47c25a727
felipe021793 <l.felipe.vanegas@gmail.com>
2019-09-19 22:34:13 -0500

    normalizado

--- src/index.js
+++ src/index.js
@@ -6,23 +6,26 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         //Below we defined the properties that will be used
-        this.state = { lat: 40,errorMessage: '' };
+        this.state = { lat: 0, errorMessage: '' };
         window.navigator.geolocation.getCurrentPosition(
-            position =>{
-                this.setState({lat: position.coords.latitude})
+            position => {
+                this.setState({ lat: position.coords.latitude })
             },
             (err) => {
-                this.setState({errorMessage: err.message})
+                this.setState({ errorMessage: err.message })
             }
         );
     }
     render() {
-        return (
-        <div>Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-        </div>
-        );
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error {this.state.errorMessage}</div>
+        } else if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude {this.state.lat}</div>
+        } else{
+            return <div>Loading ...</div>
+        }
+
     }
 }
 ReactDOM.render(
